f9a5f82eb3a39a91724e34ade5d284651dfc6ba6
Shina Adeleye <adeleyeshina4@gmail.com>
2025-05-31 15:53:35 -0700

    NavBar corrected successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import NavBar from './component/Navbar'
+import NavBar from './component/NavBar'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
