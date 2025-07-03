44bdd2917a69ae5efe52d68961b7a16059f1a0da
Shina Adeleye <adeleyeshina4@gmail.com>
2025-07-03 13:45:12 -0700

    spinner updated

--- src/component/LoadingSpinner.jsx
+++ src/component/LoadingSpinner.jsx
@@ -3,8 +3,8 @@ import { ImSpinner3 } from 'react-icons/im'
 
 const LoadingSpinner = () => {
   return (
-    <div className='h-screen bg-secondary fixed w-screen z-50'>
-        <ImSpinner3 size={90} className='animate-spin absolute top-1/2 left-1/2 -translate-1/2' fill='#F5871F' />
+    <div className='h-screen bg-secondary fixed w-full '>
+      <ImSpinner3 size={90} className='animate-spin absolute top-1/2 left-1/2 -translate-1/2' fill='#F5871F' />
     </div>
   )
 }
